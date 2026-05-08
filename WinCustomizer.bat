51929fa4bf65bd0c9dbfed7bdf4949fad6db4fbf
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2026-05-08 14:43:23 +0200

    Add main script

---
+++ WinCustomizer.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:menu
+
+cls
+echo ===========================================
+echo                WinCustomizer
+echo ===========================================
+
+set /p choice=Scegli un'opzione valida: 
