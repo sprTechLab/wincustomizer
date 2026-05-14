7c7f08b26ec803d95b75a7dc3b3cae3e1a2b4622
sprTechLab <sprocca09@gmail.com>
2026-05-14 14:40:02 +0200

    Update WinCustomizer.bat

--- WinCustomizer.bat
+++ WinCustomizer.bat
@@ -80,3 +80,50 @@ if "%subchoice%"=="0"  goto menu
 echo Scelta non valida.
 timeout /t 2 >nul
 goto manutenzione
+
+:privacy
+cls
+echo ======================================================
+echo             Privacy & Telemetria - WinCustomizer
+echo ======================================================
+echo [1]  Disabilita telemetria di base (Esperienza utente)
+echo [2]  Disabilita Cortana e ricerca web nel menu Start
+echo [3]  Disabilita ID annunci (Advertising ID)
+echo [4]  Disabilita feedback e diagnostica (Frequenza)
+echo [5]  Disabilita tracciamento avvio app
+echo [6]  Disabilita sensore memoria (Storage Sense)
+echo [7]  Disabilita cronologia attività (Activity Feed)
+echo [8]  Disabilita servizi di geolocalizzazione
+echo [9]  Disabilita tracciamento dei file recenti
+echo [10] Disabilita telemetria di Microsoft Edge
+echo [11] Disabilita "Esperienze condivise" (Project Rome)
+echo [12] Disabilita telemetria di Office (Se installato)
+echo [13] Disabilita Wifi-Sense (Condivisione automatica)
+echo [14] Disabilita segnalazione errori Windows (WER)
+echo [15] Rimuovi permessi fotocamera per app in background
+echo [16] Rimuovi permessi microfono per app in background
+echo [0]  Torna al menu principale
+echo ======================================================
+set /p privchoice=Seleziona un'operazione (0-16): 
+
+if "%privchoice%"=="1"  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="2"  reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f & reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "BingSearchEnabled" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="3"  reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="4"  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d 1 /f & pause & goto privacy
+if "%privchoice%"=="5"  reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackProgs" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="6"  reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "0" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="7"  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="8"  powershell -Command "Stop-Service lfsvc; Set-Service lfsvc -StartupType Disabled" & pause & goto privacy
+if "%privchoice%"=="9"  reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "NoRecentDocsHistory" /t REG_DWORD /d 1 /f & pause & goto privacy
+if "%privchoice%"=="10" reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "MetricsReportingEnabled" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="11" reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CDP" /v "CdpSessionUserAuthzPolicy" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="12" reg add "HKCU\Software\Microsoft\Office\16.0\Common\Privacy" /v "PersonnelDataCollection" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="13" reg add "HKLM\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" /v "AutoConnectAllowedOEM" /t REG_DWORD /d 0 /f & pause & goto privacy
+if "%privchoice%"=="14" reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Disabled" /t REG_DWORD /d 1 /f & pause & goto privacy
+if "%privchoice%"=="15" powershell -Command "Get-AppxPackage | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register \"$($_.InstallLocation)\AppXManifest.xml\"}" & pause & goto privacy
+if "%privchoice%"=="16" reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f & pause & goto privacy
+if "%privchoice%"=="0"  goto menu
+
+echo Scelta non valida.
+timeout /t 2 >nul
+goto privacy
